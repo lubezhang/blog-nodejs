b1f1031ad37e0a16e7a1b43f5deef9bf7b56117f
lubezhang <lubezhang@gmail.com>
2014-03-17 14:37:51 +0800

    修改ejs模板的扩展名为html

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ var path = require('path');
 var express = require('express');
 var partials = require('express-partials');
 var MongoStore = require('connect-mongo')(express);
-var sessionStore = new MongoStore({
-	db : settings.db
-}, function() {
-	console.log('connect mongodb success...');
-});
+// var sessionStore = new MongoStore({
+// 	db : settings.db
+// }, function() {
+// 	console.log('connect mongodb success...');
+// });
 
 var settings = require("./models/db");
 var routes = require('./routes');
@@ -25,20 +25,22 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 // 设置页面文件路径
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 app.use(partials());
 app.set('layout', "layout");
 app.use(express.favicon());
-app.use(express.logger('dev'));
+// app.use(express.logger('dev'));
 // 解析请求体，可以从request中获取传递的参数
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser('likeshan')); 
-app.use(express.session({secret : settings.cookieSecret, store: new MongoStore({url: "mongodb://127.0.0.1:27017/weibo" }) }));
-
-// app.use(express.cookieParser('likeshan')); 
-// app.use(express.session({ secret: "andylau" }));
-
+app.use(express.session({
+	secret : settings.cookieSecret, 
+	store: new MongoStore({
+		url: "mongodb://127.0.0.1:27017/weibo" 
+	}) 
+}));
 // 加载路由信息
 app.use(routes(app));
 // 设置静态文件路径
